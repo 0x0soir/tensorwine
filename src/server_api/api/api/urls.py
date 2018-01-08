168019ecb0e5f03696c8865aeac2b215125c805f
Jorge Parrilla <isoir.21@gmail.com>
2018-01-08 13:25:46 +0100

    Test image url wine to server api for app view

--- src/server_api/api/api/urls.py
+++ src/server_api/api/api/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 
 urlpatterns = [
     path('wine_api/', include('wine_api.urls')),
     path('admin/', admin.site.urls),
 ]
+
+from django.conf.urls.static import  static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
